5d6da39ae9a49dd510876de96288083dc8cec31c
Benjamin Golder <benjamin.j.golder@gmail.com>
2011-07-28 10:45:12 -0700

    new home index page, preparing layer views

--- views.py
+++ views.py
@@ -6,6 +6,6 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response(
-            'base.html',
+            'home-index.html',
             {},
             )
