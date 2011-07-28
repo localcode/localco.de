284419dca004f3a04de9b21fe319fa6eae49c3ee
Benjamin Golder <benjamin.j.golder@gmail.com>
2011-07-28 12:07:47 -0700

    fleshing out plan for views, and layer templates

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+import layers #, sites
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,11 +7,26 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
+    # home
     (r'^$', 'localcode.views.index'),
+    (r'^about/$', 'localcode.views.about'),
+    (r'^tools/$', 'localcode.views.tools'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+    # layers
+    (r'^layers/$', 'layers.views.index'),
+    (r'^layers/upload/$', 'layers.views.upload'),
+    (r'^layers/user/$', 'layers.views.user'),
+
+    # sites
+    (r'^sites/$', 'sites.views.index'),
+    (r'^sites/create/$', 'sites.views.create'),
+    (r'^sites/preview/$', 'sites.views.preview'),
+    (r'^sites/build/$', 'sites.views.build'),
+    (r'^sites/download/$', 'sites.views.download'),
+
+    # admin
+    (r'^admin/', include(admin.site.urls)),
+    # admin documentation
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
 )
