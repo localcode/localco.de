3c9eface8eaaee4e573ad4daa2bdd1bc14e1480c
cesandoval <ce.sandoval09@gmail.com>
2012-10-11 16:45:35 -0700

    modified wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if path not in sys.path:
     sys.path.append(path)
 
